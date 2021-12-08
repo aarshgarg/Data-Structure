0daec33ff7c3bcdf238481b21926b2e1a63617d9
Aarsh <79684936+aarshgarg@users.noreply.github.com>
2021-12-08 13:24:03 +0530

    Add files via upload
    linked list

---
+++ linkedlist.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* next;
+// in this we make the user defined data type 
+// 
+};
+ void linklist(node* n)
+ {
+     while(n!=NULL)
+     {
+         cout<<n->data<<endl;
+        // n=n->next;
+
+     }
+     
+ }
+int main()
+{
+
+
+ node * head= NULL;
+ node * second= NULL;
+ node * third=NULL;
+
+ head= new node();
+ second = new node();
+ third = new node();
+ head->data=1;
+ head->next=second;
+
+second->data=3;
+second->next=third;
+third->data=4;
+third->next=NULL;
+ linklist(head);
+return 0;
+}
